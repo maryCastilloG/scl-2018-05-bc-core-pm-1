930910076e5fdce1e052febcd18c325428b5be02
maryCastilloG <maryecg@gmail.com>
2018-06-01 15:21:24 -0400

    declaro variables

--- cipher.js
+++ cipher.js
@@ -1,8 +1,24 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (string, offset) => {
+    let endmessage = "";
+    for(let i = 0; i < string.length; i++){
+      let acsii = string.charCodeAt(i);
+      let newposition = (acsii - 65 + offset) % 26;
+      let acsii1 = String.fromCharCode(newposition + 65);
+      endmessage += acsii1;
+      
+    }
+    return endmessage;
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (string, offset) => {
+    let endmessage = "";
+    for(let i = 0; i < string.length; i++){
+      let acsii = string.charCodeAt(i);
+      let newposition = (acsii - 65 - offset) % 26;
+      let acsii1 = String.fromCharCode(newposition + 65);
+      endmessage += acsii1;
+    }
+  return endmessage;
   }
+  
 }
