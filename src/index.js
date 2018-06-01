fc19fcc41c34c4d90e2985cb1680c6d10ef0b3c6
maryCastilloG <maryecg@gmail.com>
2018-06-01 15:21:24 -0400

    declaro variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-/* Acá va tu código */
+function message() {
+
+    let string = document.getElementById('mensaje').value;
+    let offset = parseInt(document.getElementById('valor').value);
+    
+    document.getElementById('decodificado').value = cipher.encode(string,offset);
+}
+
+function message1() {
+
+    let string = document.getElementById('mensaje').value;
+    let offset = parseInt(document.getElementById('valor').value);
+    
+    document.getElementById('decodificado').value = cipher.decode(string,offset);
+}
+
+    
+    
